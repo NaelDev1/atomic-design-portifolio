abff479119f7c44522987ffa6312018be18dace4
NaelDev1 <naelmorais314@gmail.com>
2026-01-30 22:20:11 -0300

    feat: fading the loader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var btnContact = document.querySelector(".jl-btn-contact");
 
+window.addEventListener("load", function () {
+  var pagePreloader = this.document.querySelector(".jl-preloader");
+  pagePreloader.classList.add("jl-fade-out");
+});
+
 btnContact.addEventListener("click", function () {
   var boxContact = document.querySelector(".jl-contact-info");
   boxContact.classList.toggle("jl-is-open");
